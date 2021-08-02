6f9463a8ddedef638c2affc2883468765756907f
Seth Broweleit <seth.broweleit@insight.com>
2021-08-02 12:39:38 -0400

    layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { HostBinding, ViewEncapsulation, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
+  @HostBinding('class')
+  classAttribute: string = 'content-between grid min-h-screen';
+
   title = 'myapp-angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AppHeaderComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  // imports: [BrowserModule,AppRoutingModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
